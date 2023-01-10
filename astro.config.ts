f82321f337fc65f1d6642d349041511ad404b92b
zhangzheheng12345@163.com <zhangzheheng12345@163.com>
2023-01-10 10:31:29 +0800

    Use netlify

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro';
-import { presetUno, presetIcons } from 'unocss';
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
+import { presetUno, presetIcons } from 'unocss'
+import vue from '@astrojs/vue'
+
+import netlify from '@astrojs/netlify/'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({
-    presets: [presetIcons({
-      scale: 1.4,
-      cdn: 'https://esm.sh'
-    }), presetUno()]
-  }), vue()]
-});
+  integrations: [
+    UnoCSS({
+      presets: [
+        presetIcons({
+          scale: 1.4,
+          cdn: 'https://esm.sh'
+        }),
+        presetUno()
+      ]
+    }),
+    vue()
+  ],
+  output: 'server',
+  adapter: netlify()
+})
