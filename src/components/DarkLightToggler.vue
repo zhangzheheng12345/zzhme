a0b96bcc54331dfdc5335e261e8d1c62954cf48b
zhangzheheng12345@163.com <zhangzheheng12345@163.com>
2023-02-26 21:33:28 +0800

    Update dark & light mode toggler and styles

--- src/components/DarkLightToggler.vue
+++ src/components/DarkLightToggler.vue
@@ -1,12 +1,24 @@
 <template>
   <button
-    class="m-5px text-18px dark:bg-white"
-    :class="isDark ? 'i-uil-moon' : 'i-uil-sun'"
+    class="border-2px rounded-15px m-5px h-30px w-50px dark:border-white"
     :title="isDark ? 'Toggle to light mode' : 'Toggle to dark mode'"
     @click="($event) => toggleDark()"
-  ></button>
+  >
+    <span
+      class="inline-block text-18px dark:bg-white scale-80"
+      :class="
+        isDark ? 'i-uil-moon translate-x-11px' : 'i-uil-sun  translate-x--10px'
+      "
+    ></span>
+  </button>
 </template>
 
 <script setup lang="ts">
 import { isDark, toggleDark } from '../logic/isDark'
 </script>
+
+<style scoped>
+span {
+  transition: transform 0.3s ease-in-out;
+}
+</style>
